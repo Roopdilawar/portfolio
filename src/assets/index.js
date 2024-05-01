ea25e8a949e25378e5bb49be804a9a2c8c92d03d
roopdilawar <roopdilawar@gmail.com>
2024-04-30 22:09:35 -0600

    Work section updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -30,6 +30,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import hcl from "./company/hcl.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -61,6 +62,7 @@ export {
   shopify,
   starbucks,
   tesla,
+  hcl,
   carrent,
   jobit,
   tripguide,
